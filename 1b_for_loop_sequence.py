8da4d140fbe9c031809e5b37c1ee4c7913db3ec5
veesam bharath <veesam.veesam2555@gmail.com>
2025-11-07 05:56:21 +0530

    Add for loop to print number sequence
    Program prints a sequence of numbers starting from 8, incrementing by 3.

---
+++ 1b_for_loop_sequence.py
@@ -0,0 +1,6 @@
+# Program that uses a for loop to print numbers: 8, 11, 14, 17, 20, ..., 83, 86, 89
+# The sequence starts at 8 and increments by 3 each time
+
+for i in range(8, 90, 3):
+    print(i, end=" ")
+print()  # New line at the end
