f5dbebb53b2cabab5be6ffb004d47c7d2cdb0ebe
veesam bharath <veesam.veesam2555@gmail.com>
2025-11-07 05:43:49 +0530

    Add kg to pounds conversion program

---
+++ 1a_kg_to_pounds.py
@@ -0,0 +1,7 @@
+# Program to convert kilograms to pounds
+# There are 2.2 pounds in a kilogram
+
+weight_kg = float(input("Enter weight in kilograms: "))
+weight_pounds = weight_kg * 2.2
+
+print(f"{weight_kg} kilograms is equal to {weight_pounds} pounds")
