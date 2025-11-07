45b57008b8154fb551f790b135b06f92f80a92b9
veesam bharath <veesam.veesam2555@gmail.com>
2025-11-07 06:04:28 +0530

    Add string splitting functionality in Python

---
+++ 2_split_string.py
@@ -0,0 +1,8 @@
+# Split a string into array of characters in Python
+
+string = input("Enter a string: ")
+char_array = list(string)
+
+print("Original string:", string)
+print("Character array:", char_array)
+print("Number of characters:", len(char_array))
